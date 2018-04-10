ebc7a01e007445766f367777ab2f38e81bb70029
Simon Gottstein <simon.gottstein@students.fhnw.ch>
2018-04-10 13:15:33 +0200

    added tests for ascii-converter

--- src/test/java/utils/AsciiConverterTest.java
+++ src/test/java/utils/AsciiConverterTest.java
@@ -1,4 +1,16 @@
 package test.java.utils;
 
+import org.junit.jupiter.api.Test;
+
 public class AsciiConverterTest {
+
+    @Test
+    public void convertAsciiToBinary() {
+
+        String asciicode = "01100001 01100010 01100011";
+
+
+
+
+    }
 }
