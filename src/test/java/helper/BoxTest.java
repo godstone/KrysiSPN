281fe324a6cad93371d00055734e03c82e4ae7df
Simon Gottstein <simon.gottstein@students.fhnw.ch>
2018-04-10 13:16:37 +0200

    Add new stuff

--- src/test/java/helper/BoxTest.java
+++ src/test/java/helper/BoxTest.java
@@ -1,4 +1,4 @@
-package test.java.helper;
+package helper;
 
 public class BoxTest {
 }
--- src/test/java/utils/AsciiConverterTest.java
+++ src/test/java/utils/AsciiConverterTest.java
@@ -1,15 +1,31 @@
-package test.java.utils;
+package utils;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static utils.AsciiConverter.AsciiToBinary;
+import static utils.AsciiConverter.BinaryToAscii;
+
 
 public class AsciiConverterTest {
 
     @Test
     public void convertAsciiToBinary() {
 
-        String asciicode = "01100001 01100010 01100011";
+        assertEquals("01100001", AsciiToBinary("a"));
+        assertEquals("01000001", AsciiToBinary("A"));
+        assertEquals("011000010110001001100011", AsciiToBinary("abc"));
+        assertEquals("01100101011010010110111000100000011010110110110001100101011010010110111001100101011100100010000001110100011001010111001101110100", AsciiToBinary("ein kleiner test"));
 
+    }
 
+    @Test
+    public void convertBinaryToAscii() {
+
+        assertEquals("a", BinaryToAscii("01100001"));
+        assertEquals("A", BinaryToAscii("01000001"));
+        assertEquals("abc", BinaryToAscii("011000010110001001100011"));
+        assertEquals("ein kleiner test", BinaryToAscii("01100101011010010110111000100000011010110110110001100101011010010110111001100101011100100010000001110100011001010111001101110100"));
 
 
     }
