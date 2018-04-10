0c362bc91ab4491bce7080d70cdf64321312e933
Simon Gottstein <simon.gottstein@students.fhnw.ch>
2018-04-10 14:36:12 +0200

    Added CTR-Algorithm (not functional) Changed AsciiConterter Function-Names Added BoxTests

--- src/test/java/helper/BoxTest.java
+++ src/test/java/helper/BoxTest.java
@@ -1,4 +1,31 @@
 package helper;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+
 public class BoxTest {
+
+
+    // sbox test init
+    //
+
+
+    @Before
+    public void setUp() throws Exception {
+        SBox sbox = new SBox;
+    }
+
+
+    @Test
+    public void useSBoxGetOriginal() {
+        Assert.assertEquals("0010",sbox.getFromOriginal("1101"));
+    }
+
+    @Test
+    public void useSBoxGetInverse() {
+        Assert.assertEquals("1101",sbox.getFromOriginal("0010"));
+    }
+
 }
--- src/test/java/utils/AsciiConverterTest.java
+++ src/test/java/utils/AsciiConverterTest.java
@@ -3,25 +3,25 @@ package utils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static utils.AsciiConverter.AsciiToBinary;
-import static utils.AsciiConverter.BinaryToAscii;
+import static utils.AsciiConverter.asciiToBinary;
+import static utils.AsciiConverter.binaryToAscii;
 
 
 public class AsciiConverterTest {
 
     @Test
     public void convertAsciiToBinary() {
-        Assert.assertEquals("01100001", AsciiToBinary("a"));
-        Assert.assertEquals("01000001", AsciiToBinary("A"));
-        Assert.assertEquals("011000010110001001100011", AsciiToBinary("abc"));
-        Assert.assertEquals("01100101011010010110111000100000011010110110110001100101011010010110111001100101011100100010000001110100011001010111001101110100", AsciiToBinary("ein kleiner test"));
+        Assert.assertEquals("01100001", asciiToBinary("a"));
+        Assert.assertEquals("01000001", asciiToBinary("A"));
+        Assert.assertEquals("011000010110001001100011", asciiToBinary("abc"));
+        Assert.assertEquals("01100101011010010110111000100000011010110110110001100101011010010110111001100101011100100010000001110100011001010111001101110100", asciiToBinary("ein kleiner test"));
     }
 
     @Test
     public void convertBinaryToAscii() {
-        Assert.assertEquals("a", BinaryToAscii("01100001"));
-        Assert.assertEquals("A", BinaryToAscii("01000001"));
-        Assert.assertEquals("abc", BinaryToAscii("011000010110001001100011"));
-        Assert.assertEquals("ein kleiner test", BinaryToAscii("01100101011010010110111000100000011010110110110001100101011010010110111001100101011100100010000001110100011001010111001101110100"));
+        Assert.assertEquals("a", binaryToAscii("01100001"));
+        Assert.assertEquals("A", binaryToAscii("01000001"));
+        Assert.assertEquals("abc", binaryToAscii("011000010110001001100011"));
+        Assert.assertEquals("ein kleiner test", binaryToAscii("01100101011010010110111000100000011010110110110001100101011010010110111001100101011100100010000001110100011001010111001101110100"));
     }
 }
